1021559792c6276c4956dd209271349f686732cb
Ayush Kumar Singh <ayush2006singh2622@gmail.com>
2026-05-08 21:30:23 +0530

    feat: define RESTful routes for task management

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createTask, getTasks, updateTask, completeTask, getStats } from '../controllers/taskController';
+import { createTask, getTasks, updateTask, completeTask, getStats, deleteTask } from '../controllers/taskController';
 import { authMiddleware } from '../middleware/authMiddleware';
 import { roleMiddleware } from '../middleware/roleMiddleware';
 import { validate } from '../middleware/validateMiddleware';
@@ -14,5 +14,6 @@ router.get('/', getTasks);
 router.post('/', roleMiddleware('ADMIN'), validate(createTaskSchema), createTask);
 router.patch('/:id', updateTask);
 router.patch('/:id/complete', completeTask);
+router.delete('/:id', roleMiddleware('ADMIN'), deleteTask);
 
 export default router;
